0cdc034135a173cbf70a153684c8000cf8c2837b
SIDsklv <unit0007@o2.pl>
2017-05-20 15:51:03 +0200

    Utworzenie modułu
    (chyba działa)`

---
+++ StanLakeside_0.0.0.Jackson_County/Functions/modules/Gangs/Functions.hpp
@@ -0,0 +1,22 @@
+class Gangs_Module  {
+	tag = "ClientModules_Gangs";
+	class GangsInit
+	{
+		file = "Functions\modules\Gangs";
+		class initGangs {};
+	};
+	class Gangs
+	{
+		file = "Functions\modules\Gangs\Functions";
+		class fn_checkGang {}; // tu dodajemy funkcje np. fn_nazwafunkcji.sqf
+		class  {};
+		class  {};
+		class  {};
+		class  {};
+		class  {};
+		class  {};
+		class  {};
+		class  {};
+		class  {};
+	};
+};
---
+++ StanLakeside_0.0.0.Jackson_County/Functions/modules/Gangs/fn_initGangs.sqf
@@ -0,0 +1,4 @@
+gangList = [76561198061433788];
+
+Gangs_inited = true; // na koncu
+diag_log "Gangs Module inited"; // na koncu
